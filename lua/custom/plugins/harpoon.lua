a57b9ba3cb7d4595ba0c9bd58dbe0079043b011c
Otto Reimers <otto.reimers@frojd.se>
2025-03-24 20:01:10 +0100

    Update harpoon to harpoon2

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,5 +1,9 @@
 return {
   'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
   config = function()
     require('harpoon').setup {
       -- options
